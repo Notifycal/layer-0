05593a081747f63b917d8bf1004d072904dcc744
dsiguero <26867647+dsiguero@users.noreply.github.com>
2024-04-02 22:01:53 +0200

    feat: protect main branch

--- ci/org-repos.tf
+++ ci/org-repos.tf
@@ -5,6 +5,10 @@ variable "github_organization_name" {
 
 # The data source will return a maximum of 1000 repositories as documented in official API docs.
 data "github_repositories" "all_repos" {
-  query = "org:${var.github_organization_name}"
+  query = "org:${var.github_organization_name} archived:false"
   include_repo_id = true
+}
+
+output "all_org_repos" {
+  value = data.github_repositories.all_repos.names
 }
