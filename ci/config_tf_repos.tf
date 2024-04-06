687d199683f2f038b2dc1dc070c1994543a34da8
Daniel Siguero <26867647+dsiguero@users.noreply.github.com>
2024-04-06 15:57:27 +0200

    feat(ci): split deployable and TF repos (#7)
    All tofu modules might require to resolve private git repos (other
    private submodules). But that doesn't necessarily mean they need to be
    deployed to AWS.
    
    For example: `static-landing` isn't deployable on its own, but relies on
    other private TF modules like `tofu-module-static-website`.

---
+++ ci/config_tf_repos.tf
@@ -0,0 +1,37 @@
+## stuff that's common to all TF repos. 
+## For example, TF repos will likely need to access other private TF module repos
+## This does not do anything cloud-related
+
+locals {
+  # this array can be used to do partial matches too. ie: `tofu-module-`
+  include_tf_repos = [
+    "docs-internal",
+    "static-landing"
+    # infra repo goes here
+  ]
+  
+  # Keep repos that match any regex in local.include_repos
+  tf_repos = toset([
+    for repo in data.github_repositories.all_repos.names: repo if anytrue([
+      for included_tf_repo in local.include_tf_repos: length(regexall(included_tf_repo, repo)) > 0
+    ])
+  ])
+}
+
+
+## Github App secrets
+resource "github_actions_secret" "cicd_app_id" {
+  for_each = local.tf_repos
+
+  repository       = each.value
+  secret_name      = "NOTIFYCAL_CICD_APP_ID"
+  plaintext_value  = data.aws_ssm_parameter.cicd_app_id.value
+}
+
+resource "github_actions_secret" "cicd_app_secret" {
+  for_each = local.tf_repos
+
+  repository       = each.value
+  secret_name      = "NOTIFYCAL_CICD_APP_SECRET"
+  plaintext_value  = data.aws_ssm_parameter.cicd_app_secret.value
+}
