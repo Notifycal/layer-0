ff9bbd2ecfd8eeeef1850aace5734e05a6450d23
Daniel Siguero <26867647+dsiguero@users.noreply.github.com>
2025-01-28 21:48:06 +0100

    feat: let dependabot access CI/CD app secrets (#19)

--- ci/config_all_repos.tf
+++ ci/config_all_repos.tf
@@ -15,6 +15,23 @@ resource "github_actions_secret" "cicd_app_secret" {
   plaintext_value = data.aws_ssm_parameter.cicd_app_secret.value
 }
 
+## Need to create them as dependabot secrets too so dependabot can access them
+resource "github_dependabot_secret" "cicd_app_id" {
+  for_each = toset(data.github_repositories.all_repos.names)
+
+  repository      = each.value
+  secret_name     = "NOTIFYCAL_CICD_APP_ID"
+  plaintext_value = data.aws_ssm_parameter.cicd_app_id.value
+}
+
+resource "github_dependabot_secret" "cicd_app_secret" {
+  for_each = toset(data.github_repositories.all_repos.names)
+
+  repository      = each.value
+  secret_name     = "NOTIFYCAL_CICD_APP_SECRET"
+  plaintext_value = data.aws_ssm_parameter.cicd_app_secret.value
+}
+
 resource "github_dependabot_secret" "dependabot_pat" {
   # This is enabled for all repos by default
   for_each = toset(data.github_repositories.all_repos.names)
