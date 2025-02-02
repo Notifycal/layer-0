22e796bc6b743c8c44ff03f5b33489bfcb59da49
dsiguero <26867647+dsiguero@users.noreply.github.com>
2025-02-02 11:24:54 +0100

    feat: enforce status checks before merging

--- ci/config_all_repos.tf
+++ ci/config_all_repos.tf
@@ -40,20 +40,3 @@ resource "github_dependabot_secret" "dependabot_pat" {
   secret_name     = "DEPENDABOT_PAT"
   plaintext_value = var.dependabot_pat
 }
-
-resource "github_branch_protection" "this" {
-  for_each = var.enable_branch_protection ? toset(data.github_repositories.all_repos.names) : []
-
-  repository_id = each.key
-  pattern       = "main"
-
-  required_pull_request_reviews {
-    dismiss_stale_reviews           = true
-    required_approving_review_count = 0
-  }
-
-  required_status_checks {
-    contexts = []
-    strict   = false
-  }
-}
