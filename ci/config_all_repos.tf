1a8b0fda9efc990c3449ebe4caed8cdf7aa2017c
dsiguero <26867647+dsiguero@users.noreply.github.com>
2025-04-13 11:31:18 +0200

    feat: add ability to ignore certain repos (research)

--- ci/config_all_repos.tf
+++ ci/config_all_repos.tf
@@ -1,6 +1,14 @@
+locals {
+  ignore_repos = [
+    "event-viewer-ai-bolt.new",
+    "event-viewer-ai-lovable.dev",
+  ]
+  all_repos = setsubtract(data.github_repositories.all_repos.names, local.ignore_repos)
+}
+
 ## Github App secrets. Required for issue handing stuff
 resource "github_actions_secret" "cicd_app_id" {
-  for_each = toset(data.github_repositories.all_repos.names)
+  for_each = local.all_repos
 
   repository      = each.value
   secret_name     = "NOTIFYCAL_CICD_APP_ID"
@@ -8,7 +16,7 @@ resource "github_actions_secret" "cicd_app_id" {
 }
 
 resource "github_actions_secret" "cicd_app_secret" {
-  for_each = toset(data.github_repositories.all_repos.names)
+  for_each = local.all_repos
 
   repository      = each.value
   secret_name     = "NOTIFYCAL_CICD_APP_SECRET"
@@ -17,7 +25,7 @@ resource "github_actions_secret" "cicd_app_secret" {
 
 ## Need to create them as dependabot secrets too so dependabot can access them
 resource "github_dependabot_secret" "cicd_app_id" {
-  for_each = toset(data.github_repositories.all_repos.names)
+  for_each = local.all_repos
 
   repository      = each.value
   secret_name     = "NOTIFYCAL_CICD_APP_ID"
@@ -25,7 +33,7 @@ resource "github_dependabot_secret" "cicd_app_id" {
 }
 
 resource "github_dependabot_secret" "cicd_app_secret" {
-  for_each = toset(data.github_repositories.all_repos.names)
+  for_each = local.all_repos
 
   repository      = each.value
   secret_name     = "NOTIFYCAL_CICD_APP_SECRET"
@@ -34,7 +42,7 @@ resource "github_dependabot_secret" "cicd_app_secret" {
 
 resource "github_dependabot_secret" "dependabot_pat" {
   # This is enabled for all repos by default
-  for_each = toset(data.github_repositories.all_repos.names)
+  for_each = local.all_repos
 
   repository      = each.value
   secret_name     = "DEPENDABOT_PAT"
