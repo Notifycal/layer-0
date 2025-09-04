55c52fc9366434b68901637d844f42a7b04c5930
SeRGiOJoKeR11 <sergio.anger@gmail.com>
2025-09-04 11:26:20 +0200

    group dns stuff (#35)
    Co-authored-by: Daniel Siguero <26867647+dsiguero@users.noreply.github.com>

--- cloudflare/acm_cert.tf
+++ cloudflare/acm_cert.tf
@@ -1,18 +1,12 @@
 module "aws_acm_star_notifycal_ssl_cert" {
-  source = "git@github.com:Notifycal/tofu-module-acm-cert.git?ref=v0.2.0"
+  source = "../../tofu-module-acm-cert"
 
   domain_name = "*.notifycal.com"
-
-  create_dns_validation_records = false
-}
-
-resource "cloudflare_record" "dns_validate" {
-  for_each = module.aws_acm_star_notifycal_ssl_cert.certificate_validation_dns_records
-
-  zone_id = cloudflare_zone.zones["notifycal.com"].id
-
-  name    = each.value.name
-  content = each.value.record
-  type    = each.value.type
-  proxied = false
+  dns_validation_config = {
+    vendor = "cloudflare"
+    cloudflare = {
+      zone_id = cloudflare_zone.zones["notifycal.com"].id
+      proxied = false
+    }
+  }
 }
