6fb59563dcd535397f0b03ada9fa58287dc21801
dsiguero <26867647+dsiguero@users.noreply.github.com>
2025-09-05 15:22:30 +0200

    fix key

--- ci/branch_protection_all_repos.tf
+++ ci/branch_protection_all_repos.tf
@@ -13,7 +13,7 @@ locals {
     }
     environments = {
       filter-envs = "filter-envs"
-      tofu        = "postflight"
+      postflight  = "postflight"
     }
     frontend = {
       preflight = "preflight"
