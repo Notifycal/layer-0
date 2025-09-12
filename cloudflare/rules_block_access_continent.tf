95d3255ec77702e996da36f4ff1173f9e6885fe9
dsiguero <26867647+dsiguero@users.noreply.github.com>
2025-09-12 16:23:19 +0200

    fmt

--- cloudflare/rules_block_access_continent.tf
+++ cloudflare/rules_block_access_continent.tf
@@ -1,23 +1,23 @@
 locals {
   blocked_continents = {
-    africa = "AF"
+    africa    = "AF"
     antartica = "AN"
-    asia = "AS"
-    oceania = "OC"
-    tor = "T1"
+    asia      = "AS"
+    oceania   = "OC"
+    tor       = "T1"
   }
-  blocked_continents_rule_query = "(ip.src.continent in {${join(" ", formatlist("\"%s\"",values(local.blocked_continents)))}})"
+  blocked_continents_rule_query = "(ip.src.continent in {${join(" ", formatlist("\"%s\"", values(local.blocked_continents)))}})"
 }
 
 resource "cloudflare_ruleset" "continent_block" {
   for_each = cloudflare_zone.zones
 
   zone_id = each.value.id
-  name = "ruleset"
-  kind = "zone"
-  phase = "http_request_firewall_custom"
+  name    = "ruleset"
+  kind    = "zone"
+  phase   = "http_request_firewall_custom"
 
-   rules = [{
+  rules = [{
     enabled     = true
     action      = "block"
     description = "BlockContinentAccess"
